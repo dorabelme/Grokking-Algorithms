bf1a14e3b50f2136ded009a1123266c90eff530d
Dora Belme <contact.dorabelme@gmail.com>
2019-03-28 12:53:24 -0700

    Add files

---
+++ chapter4_maxrec.py
@@ -0,0 +1,16 @@
+def max(list):
+	if len(list) == 2:
+		if list[0] > list[1]:
+			return list[0]
+		else:
+			return list[1]
+	sub_max = max(list[1:])
+	if list[0] > sub_max:
+		return list[0]
+	else:
+		return sub_max
+	
+
+
+print(max([1,2,3,4,15,99,67]))
+
---
+++ chapter9_dynamicprogramming.py
@@ -0,0 +1,12 @@
+if word_a[i] == word_b[j]:
+	cell[i][j] == cell[i-1][j-1] + 1
+else:
+	cell[i][j] = 0
+
+if word_a[i] == word_b[j]:
+	cell[i][j] = cell[i-1][j-1] + 1
+else:
+	cell[i][j] = max(cell[i-1][j], cell[i][j-1])
+
+
+	
